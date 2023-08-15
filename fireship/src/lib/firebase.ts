7192c4ce1923897e2dda7e32146b8fe672919b27
NVC541 <naveenmukkatt@gmail.com>
2023-08-15 15:49:57 -0500

    OAuth enabled

--- fireship/src/lib/firebase.ts
+++ fireship/src/lib/firebase.ts
@@ -1,8 +1,9 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDnRkBXcSRV7yuez7VkYxi10Pwp3Jv2naw",
@@ -18,3 +19,32 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+function userStore() {
+    let unsubscribe: () => void;
+    
+    if (!auth || !globalThis.window) {
+        console.warn('Auth not initialized or not in a browser.');
+        const { subscribe } = writable<User | null>(null);
+
+        return {
+            subscribe,
+        }
+    }
+    
+    const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+        unsubscribe = onAuthStateChanged(auth, (user) => {
+            set(user);
+        });
+
+        return () => subscribe();
+    });
+
+    return {
+        subscribe,
+    };
+
+
+}
+
+export const user = userStore();
